bdb58cf4e99a0e84ca0c6fe865092954646eca51
hasin01 <mrsemenav5@gmail.com>
2023-06-13 15:06:03 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import { FetchImages } from './Section';
+import { Rection } from "./Rection2";
+import userData from '../users.json'
+
+
 export const App = () => {
+
+ 
+  
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+<>
+<div className="sectionapp">
+ {userData.map(user =>(
+
+<Rection key = {user.id} name ={user.name} surname ={user.surname} age ={user.age}/>
+
+
+ ))}
+
+
+
+
+</div>
+<fetchImages/>
+</>
+
   );
 };
